c646874b3a0f94cfc7767ee4ad676d89dc7dfd05
bozana <bozana.bokan@fu-berlin.de>
2015-10-02 14:40:55 +0200

    default EN translation plugin

---
+++ plugins/generic/defaultENTranslation/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file index.php
+ *
+ * Copyright (c) 2013-2015 Simon Fraser University Library
+ * Copyright (c) 2003-2015 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @package plugins.generic.defaultENTranslation
+ *
+ * Wrapper for the plugin.
+ *
+ */
+
+require_once('DefaultENTranslationPlugin.inc.php');
+return new DefaultENTranslationPlugin();
+
+?>
