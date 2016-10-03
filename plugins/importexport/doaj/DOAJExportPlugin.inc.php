ce46ce6f55acf6a9098768d99db08ab6533669df
ajnyga <ajnyga@gmail.com>
2016-10-03 22:55:46 +0300

    Add depositXML

--- plugins/importexport/doaj/DOAJExportPlugin.inc.php
+++ plugins/importexport/doaj/DOAJExportPlugin.inc.php
@@ -17,6 +17,14 @@ import('classes.plugins.PubObjectsExportPlugin');
 
 define('DOAJ_XSD_URL', 'http://www.doaj.org/schemas/doajArticles.xsd');
 
+define('DOAJ_API_DEPOSIT_OK', 201);
+
+define('DOAJ_API_URL', 'http://doaj.org/api/v1/');
+define('DOAJ_API_URL_DEV', 'http://testdoaj.cottagelabs.com/api/v1/');
+# We should probably use bulk articles: "A list/array of article JSON objects that you would like to create or update"?
+# /api/v1/articles 
+# /api/v1/bulk/articles 
+
 class DOAJExportPlugin extends PubObjectsExportPlugin {
 	/**
 	 * Constructor
@@ -92,16 +100,53 @@ class DOAJExportPlugin extends PubObjectsExportPlugin {
 	/**
 	 * @copydoc PubObjectsExportPlugin::depositXML()
 	 */
-	function depositXML($objects, $context, $filename) {
-		// Deposit was received
-		$result = true;
-		foreach ($objects as $object) {
-			// set the status
-			$object->setData($this->getDepositStatusSettingName(), EXPORT_STATUS_REGISTERED);
-			// Update the object
-			$this->updateObject($object);
+function depositXML($objects, $context, $filename) {
+		
+		$curlCh = curl_init();
+		if ($httpProxyHost = Config::getVar('proxy', 'http_host')) {
+			curl_setopt($curlCh, CURLOPT_PROXY, $httpProxyHost);
+			curl_setopt($curlCh, CURLOPT_PROXYPORT, Config::getVar('proxy', 'http_port', '80'));
+			if ($username = Config::getVar('proxy', 'username')) {
+				curl_setopt($curlCh, CURLOPT_PROXYUSERPWD, $username . ':' . Config::getVar('proxy', 'password'));
+			}
 		}
+		
+		curl_setopt($curlCh, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curlCh, CURLOPT_POST, true);
+		curl_setopt($curlCh, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+		# filename; Article JSON example https://github.com/DOAJ/harvester/blob/9b59fddf2d01f7c918429d33b63ca0f1a6d3d0d0/service/tests/fixtures/article.py
+		curl_setopt($curlCh, CURLOPT_POSTFIELDS, $jsondata );
+
+		# DOAJ API Key. Need to do settings for saving this. https://testdoaj.cottagelabs.com/api/v1/bulk/articles?api_key=keyhash
+		$api_key = $this->getSetting($context->getId(), 'api_key');
+		
+		$params = 'api_key=' . $api_key;
+		curl_setopt(
+			$curlCh,
+			CURLOPT_URL,
+			DOAJ_API_URL_DEV . (strpos(DOAJ_API_URL_DEV,'?')===false?'?':'&') . $params
+		);
+		
+		$response = curl_exec($curlCh);
+
+		if ($response === false) {
+			$result = array(array('plugins.importexport.doaj.register.error.mdsError', 'No response from server.'));
+		} elseif ( $status = curl_getinfo($curlCh, CURLINFO_HTTP_CODE) != DOAJ_API_DEPOSIT_OK ) {
+			$result = array(array('plugins.importexport.doaj.register.error.mdsError', "$status - $response"));
+		} else {
+			// Deposit was received
+			$result = true;
+			foreach ($objects as $object) {
+				// set the status
+				$object->setData($this->getDepositStatusSettingName(), EXPORT_STATUS_REGISTERED);
+				// Update the object
+				$this->updateObject($object);
+			}
+		}
+		curl_close($curlCh);
 		return $result;
+		
 	}
 }
 
