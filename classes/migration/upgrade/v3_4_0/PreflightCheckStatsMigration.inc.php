2dceefee91278e4055b1505b0fa16c8a2f414276
Bozana Bokan <bozana.bokan@posteo.net>
2022-05-16 15:08:53 +0200

    fixes - pfeflight check

--- classes/migration/upgrade/v3_4_0/PreflightCheckStatsMigration.inc.php
+++ classes/migration/upgrade/v3_4_0/PreflightCheckStatsMigration.inc.php
@@ -14,39 +14,30 @@
 namespace APP\migration\upgrade\v3_4_0;
 
 use APP\core\Application;
-use APP\statistics\StatisticsHelper;
 use Illuminate\Support\Facades\DB;
 use PKP\db\DAORegistry;
-use PKP\file\FileManager;
 
 class PreflightCheckStatsMigration extends \PKP\migration\Migration
 {
     /**
      * Run the migrations.
+     *
+     * assoc_object_type and assoc_object_id, pkp_section_id will not be considered here, because they are not relevant for the migration
      */
     public function up(): void
     {
         try {
-            // Check if there are unprocessed old usage event log files
-            $this->checkUsageEventLogFiles();
-
             // Clean orphaned metrics issue IDs
             // as m.assoc_id
             $orphanedIds = DB::table('metrics AS m')->leftJoin('issues AS i', 'm.assoc_id', '=', 'i.issue_id')->where('m.assoc_type', '=', Application::ASSOC_TYPE_ISSUE)->whereNull('i.issue_id')->distinct()->pluck('m.assoc_id');
             foreach ($orphanedIds as $issueId) {
-                $this->_installer->log("Removing orphaned metrics issue ID {$issueId}.");
+                $this->_installer->log("Removing stats for issue {$issueId} because no issue with that ID could be found.");
                 DB::table('metrics')->where('assoc_type', '=', Application::ASSOC_TYPE_ISSUE)->where('assoc_id', '=', $issueId)->delete();
-            }
-            // as m.assoc_object_id
-            $orphanedIds = DB::table('metrics AS m')->leftJoin('issues AS i', 'm.assoc_object_id', '=', 'i.issue_id')->where('m.assoc_object_type', '=', Application::ASSOC_TYPE_ISSUE)->whereNull('i.issue_id')->distinct()->pluck('m.assoc_object_id');
-            foreach ($orphanedIds as $issueId) {
-                $this->_installer->log("Removing orphaned metrics issue ID {$issueId}.");
-                DB::table('metrics')->where('assoc_object_type', '=', Application::ASSOC_TYPE_ISSUE)->where('assoc_object_id', '=', $issueId)->delete();
             }
             // Clean orphaned metrics issue galley IDs
             $orphanedIds = DB::table('metrics AS m')->leftJoin('issue_galleys AS ig', 'm.assoc_id', '=', 'ig.galley_id')->where('m.assoc_type', '=', Application::ASSOC_TYPE_ISSUE_GALLEY)->whereNull('ig.galley_id')->distinct()->pluck('m.assoc_id');
             foreach ($orphanedIds as $issueGalleyId) {
-                $this->_installer->log("Removing orphaned metrics issue galley ID {$issueGalleyId}.");
+                $this->_installer->log("Removing stats for issue galley {$issueGalleyId} because no issue galley with that ID could be found.");
                 DB::table('metrics')->where('assoc_type', '=', Application::ASSOC_TYPE_ISSUE_GALLEY)->where('assoc_id', '=', $issueGalleyId)->delete();
             }
 
@@ -54,13 +45,13 @@ class PreflightCheckStatsMigration extends \PKP\migration\Migration
             // as assoc_id
             $orphanedIds = DB::table('metrics AS m')->leftJoin('journals AS c', 'm.assoc_id', '=', 'c.journal_id')->where('m.assoc_type', '=', Application::getContextAssocType())->whereNull('c.journal_id')->distinct()->pluck('m.assoc_id');
             foreach ($orphanedIds as $contextId) {
-                $this->_installer->log("Removing orphaned metrics journal ID {$contextId}.");
+                $this->_installer->log("Removing stats for journal {$contextId} because no journal with that ID could be found.");
                 DB::table('metrics')->where('assoc_type', '=', Application::getContextAssocType())->where('assoc_id', '=', $contextId)->delete();
             }
             // as context_id
             $orphanedIds = DB::table('metrics AS m')->leftJoin('journals AS c', 'm.context_id', '=', 'c.journal_id')->whereNull('c.journal_id')->distinct()->pluck('m.context_id');
             foreach ($orphanedIds as $contextId) {
-                $this->_installer->log("Removing orphaned metrics journal ID {$contextId}.");
+                $this->_installer->log("Removing stats for journal {$contextId} because no journal with that ID could be found.");
                 DB::table('metrics')->where('context_id', '=', $contextId)->delete();
             }
 
@@ -68,42 +59,41 @@ class PreflightCheckStatsMigration extends \PKP\migration\Migration
             // as assoc_id
             $orphanedIds = DB::table('metrics AS m')->leftJoin('submissions AS s', 'm.assoc_id', '=', 's.submission_id')->where('m.assoc_type', '=', Application::ASSOC_TYPE_SUBMISSION)->whereNull('s.submission_id')->distinct()->pluck('m.assoc_id');
             foreach ($orphanedIds as $submissionId) {
-                $this->_installer->log("Removing orphaned metrics submission ID {$submissionId}.");
+                $this->_installer->log("Removing stats for submission {$submissionId} because no submission with that ID could be found.");
                 DB::table('metrics')->where('assoc_type', '=', Application::ASSOC_TYPE_SUBMISSION)->where('assoc_id', '=', $submissionId)->delete();
             }
             // as submission_id
             $orphanedIds = DB::table('metrics AS m')->leftJoin('submissions AS s', 'm.submission_id', '=', 's.submission_id')->whereNotNull('m.submission_id')->whereNull('s.submission_id')->distinct()->pluck('m.submission_id');
             foreach ($orphanedIds as $submissionId) {
-                $this->_installer->log("Removing orphaned metrics submission ID {$submissionId}.");
+                $this->_installer->log("Removing stats for submission {$submissionId} because no submission with that ID could be found.");
                 DB::table('metrics')->where('submission_id', '=', $submissionId)->delete();
             }
 
             // Clean orphaned metrics submission file IDs
             $orphanedIds = DB::table('metrics AS m')->leftJoin('submission_files AS sf', 'm.assoc_id', '=', 'sf.submission_file_id')->where('m.assoc_type', '=', Application::ASSOC_TYPE_SUBMISSION_FILE)->whereNull('sf.submission_file_id')->distinct()->pluck('m.assoc_id');
             foreach ($orphanedIds as $submissionFileId) {
-                $this->_installer->log("Removing orphaned metrics submission file ID {$submissionFileId}.");
+                $this->_installer->log("Removing stats for submission file {$submissionFileId} because no submission file with that ID could be found.");
                 DB::table('metrics')->where('assoc_type', '=', Application::ASSOC_TYPE_SUBMISSION_FILE)->where('assoc_id', '=', $submissionFileId)->delete();
             }
             // Clean orphaned metrics submission supp file IDs
             $orphanedIds = DB::table('metrics AS m')->leftJoin('submission_files AS sf', 'm.assoc_id', '=', 'sf.submission_file_id')->where('m.assoc_type', '=', Application::ASSOC_TYPE_SUBMISSION_FILE_COUNTER_OTHER)->whereNull('sf.submission_file_id')->distinct()->pluck('m.assoc_id');
             foreach ($orphanedIds as $submissionSuppFileId) {
-                $this->_installer->log("Removing orphaned metrics submission supplementary file ID {$submissionSuppFileId}.");
+                $this->_installer->log("Removing stats for submission supplementary file {$submissionSuppFileId} because no submission supplementary file with that ID could be found.");
                 DB::table('metrics')->where('assoc_type', '=', Application::ASSOC_TYPE_SUBMISSION_FILE_COUNTER_OTHER)->where('assoc_id', '=', $submissionSuppFileId)->delete();
             }
 
             // Clean orphaned metrics representation IDs
             $orphanedIds = DB::table('metrics AS m')->leftJoin('publication_galleys AS r', 'm.representation_id', '=', 'r.galley_id')->whereNotNull('m.representation_id')->whereNull('r.galley_id')->distinct()->pluck('m.representation_id');
             foreach ($orphanedIds as $representationId) {
-                $this->_installer->log("Removing orphaned metrics article galley ID {$representationId}.");
+                $this->_installer->log("Removing stats for article galley {$representationId} because no article galley with that ID could be found.");
                 DB::table('metrics')->where('representation_id', '=', $representationId)->delete();
             }
-
-            // m.pkp_section_id will not be considered here, because it will be not migrated
 
             // Inform about probably old and not anymore supported assoc_type in the DB table metrics
             $oldAssocTypes = DB::table('metrics AS m')->whereNotIn('m.assoc_type', [Application::getContextAssocType(), Application::ASSOC_TYPE_ISSUE, Application::ASSOC_TYPE_ISSUE_GALLEY, Application::ASSOC_TYPE_SUBMISSION, Application::ASSOC_TYPE_SUBMISSION_FILE, Application::ASSOC_TYPE_SUBMISSION_FILE_COUNTER_OTHER])->distinct()->pluck('m.assoc_type');
             foreach ($oldAssocTypes as $oldAssocType) {
                 $this->_installer->log("The DB table metrics contains old and not anymore supported assoc_type {$oldAssocType} that will be not migrated to new metrics tables.");
+                $this->_installer->log("An unrecognized value was found in the assoc_type column of the metrics. The unrecognized value was {$oldAssocType}. These statistics will not be included in statistics after the upgrade is complete.");
             }
         } catch (\Exception $e) {
             if ($fallbackVersion = $this->setFallbackVersion()) {
@@ -135,45 +125,5 @@ class PreflightCheckStatsMigration extends \PKP\migration\Migration
             return $fallbackVersion;
         }
         return null;
-    }
-
-    /**
-     * Check if there are unprocessed usage event log files (older than today).
-     *
-     *  @throws \Exception
-     */
-    private function checkUsageEventLogFiles(): void
-    {
-        // Check if usageStats folders contain any log file other than the current one.
-        $processingDirPath = StatisticsHelper::getUsageStatsDirPath() . '/processing';
-        $processingDirFiles = glob($processingDirPath . '/' . '*');
-        $stageDirPath = StatisticsHelper::getUsageStatsDirPath() . '/stage';
-        $stageDirFiles = glob($stageDirPath . '/' . '*');
-        $rejectDirPath = StatisticsHelper::getUsageStatsDirPath() . '/reject';
-        $rejectDirFiles = glob($rejectDirPath . '/' . '*');
-        $usageEventLogsDirPath = StatisticsHelper::getUsageStatsDirPath() . '/usageEventLogs';
-        $usageEventLogsDirFiles = glob($usageEventLogsDirPath . '/' . '*');
-        $usageEventLogsDirFilesError = false;
-        if (is_array($usageEventLogsDirFiles)) {
-            $fileMgr = new FileManager();
-            foreach ($usageEventLogsDirFiles as $filePath) {
-                // Make sure it's a file.
-                if ($fileMgr->fileExists($filePath)) {
-                    // If file is not from today.
-                    $filename = pathinfo($filePath, PATHINFO_BASENAME);
-                    $currentDayFilename = 'usage_events_' . date('Ymd') . '.log';
-                    if ($filename != $currentDayFilename) {
-                        $usageEventLogsDirFilesError = true;
-                        break;
-                    }
-                }
-            }
-        }
-        if ((is_array($processingDirFiles) && count($processingDirFiles)) ||
-            (is_array($stageDirFiles) && count($stageDirFiles)) ||
-            (is_array($rejectDirFiles) && count($rejectDirFiles)) || $usageEventLogsDirFilesError) {
-            $msg = 'Please process all usage event log files older than today before running the upgrade. Check for log files in all usageStats folders (usageEventLogs, processing, stage and reject).';
-            throw new \Exception($msg);
-        }
     }
 }
