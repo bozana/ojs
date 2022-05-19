4854203b02fdc17b104e9392d191a54a871b92d7
Bozana Bokan <bozana.bokan@posteo.net>
2022-05-19 16:10:32 +0200

    fixes - loader

--- classes/tasks/UsageStatsLoader.inc.php
+++ classes/tasks/UsageStatsLoader.inc.php
@@ -16,29 +16,29 @@
 namespace APP\tasks;
 
 use APP\core\Application;
-use APP\statistics\UsageStatsTotalTemporaryRecordDAO;
-use APP\statistics\UsageStatsUniqueItemInvestigationsTemporaryRecordDAO;
-use APP\statistics\UsageStatsUniqueItemRequestsTemporaryRecordDAO;
+use APP\statistics\TemporaryItemInvestigationsDAO;
+use APP\statistics\TemporaryItemRequestsDAO;
+use APP\statistics\TemporaryTotalsDAO;
 use PKP\db\DAORegistry;
-use PKP\statistics\UsageStatsInstitutionTemporaryRecordDAO;
+use PKP\statistics\TemporaryInstitutionsDAO;
 use PKP\task\PKPUsageStatsLoader;
 
 class UsageStatsLoader extends PKPUsageStatsLoader
 {
-    private UsageStatsInstitutionTemporaryRecordDAO $statsInstitutionDao;
-    private UsageStatsTotalTemporaryRecordDAO $statsTotalDao;
-    private UsageStatsUniqueItemInvestigationsTemporaryRecordDAO $statsUniqueItemInvestigationsDao;
-    private UsageStatsUniqueItemRequestsTemporaryRecordDAO $statsUniqueItemRequestsDao;
+    private TemporaryInstitutionsDAO $temporaryInstitutionsDao;
+    private TemporaryTotalsDAO $temporaryTotalsDao;
+    private TemporaryItemInvestigationsDAO $temporaryItemInvestigationsDao;
+    private TemporaryItemRequestsDAO $temporaryItemRequestsDao;
 
     /**
      * Constructor.
      */
     public function __construct($args)
     {
-        $this->statsInstitutionDao = DAORegistry::getDAO('UsageStatsInstitutionTemporaryRecordDAO'); /* @var UsageStatsInstitutionTemporaryRecordDAO $statsInstitutionDao */
-        $this->statsTotalDao = DAORegistry::getDAO('UsageStatsTotalTemporaryRecordDAO'); /* @var UsageStatsTotalTemporaryRecordDAO $statsTotalDao */
-        $this->statsUniqueItemInvestigationsDao = DAORegistry::getDAO('UsageStatsUniqueItemInvestigationsTemporaryRecordDAO'); /* @var UsageStatsUniqueItemInvestigationsTemporaryRecordDAO $statsUniqueItemInvestigationsDao */
-        $this->statsUniqueItemRequestsDao = DAORegistry::getDAO('UsageStatsUniqueItemRequestsTemporaryRecordDAO'); /* @var UsageStatsUniqueItemRequestsTemporaryRecordDAO $statsUniqueItemRequestsDao */
+        $this->temporaryInstitutionsDao = DAORegistry::getDAO('TemporaryInstitutionsDAO'); /* @var TemporaryInstitutionsDAO $statsInstitutionDao */
+        $this->temporaryTotalsDao = DAORegistry::getDAO('TemporaryTotalsDAO'); /* @var TemporaryTotalsDAO $temporaryTotalsDao */
+        $this->temporaryItemInvestigationsDao = DAORegistry::getDAO('TemporaryItemInvestigationsDAO'); /* @var TemporaryItemInvestigationsDAO $temporaryItemInvestigationsDao */
+        $this->temporaryItemRequestsDao = DAORegistry::getDAO('TemporaryItemRequestsDAO'); /* @var TemporaryItemRequestsDAO $temporaryItemRequestsDao */
         parent::__construct($args);
     }
 
@@ -47,10 +47,10 @@ class UsageStatsLoader extends PKPUsageStatsLoader
      */
     protected function deleteByLoadId(string $loadId): void
     {
-        $this->statsInstitutionDao->deleteByLoadId($loadId);
-        $this->statsTotalDao->deleteByLoadId($loadId);
-        $this->statsUniqueItemInvestigationsDao->deleteByLoadId($loadId);
-        $this->statsUniqueItemRequestsDao->deleteByLoadId($loadId);
+        $this->temporaryInstitutionsDao->deleteByLoadId($loadId);
+        $this->temporaryTotalsDao->deleteByLoadId($loadId);
+        $this->temporaryItemInvestigationsDao->deleteByLoadId($loadId);
+        $this->temporaryItemRequestsDao->deleteByLoadId($loadId);
     }
 
     /**
@@ -58,12 +58,12 @@ class UsageStatsLoader extends PKPUsageStatsLoader
      */
     protected function insertTemporaryUsageStatsData(object $entry, int $lineNumber, string $loadId): void
     {
-        $this->statsInstitutionDao->insert($entry->institutionIds, $lineNumber, $loadId);
-        $this->statsTotalDao->insert($entry, $lineNumber, $loadId);
+        $this->temporaryInstitutionsDao->insert($entry->institutionIds, $lineNumber, $loadId);
+        $this->temporaryTotalsDao->insert($entry, $lineNumber, $loadId);
         if (!empty($entry->submissionId)) {
-            $this->statsUniqueItemInvestigationsDao->insert($entry, $lineNumber, $loadId);
+            $this->temporaryItemInvestigationsDao->insert($entry, $lineNumber, $loadId);
             if ($entry->assocType == Application::ASSOC_TYPE_SUBMISSION_FILE) {
-                $this->statsUniqueItemRequestsDao->insert($entry, $lineNumber, $loadId);
+                $this->temporaryItemRequestsDao->insert($entry, $lineNumber, $loadId);
             }
         }
     }
@@ -73,7 +73,7 @@ class UsageStatsLoader extends PKPUsageStatsLoader
      */
     protected function checkForeignKeys(object $entry): array
     {
-        return $this->statsTotalDao->checkForeignKeys($entry);
+        return $this->temporaryTotalsDao->checkForeignKeys($entry);
     }
 
     /**
