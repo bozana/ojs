8df6fe64cfa9d6e4d192ae6cedc91929ec2d2565
Bozana Bokan <bozana.bokan@posteo.net>
2022-05-23 15:32:46 +0200

    fixes - remove plugins by upgrade

--- classes/migration/upgrade/v3_4_0/I6782_RemovePlugins.inc.php
+++ classes/migration/upgrade/v3_4_0/I6782_RemovePlugins.inc.php
@@ -28,11 +28,23 @@ class I6782_RemovePlugins extends Migration
      */
     public function up(): void
     {
-        $fileManager = new FileManager();
-
         // Remove usageStats plugin
-        $category = 'generic';
-        $productName = 'usageStats';
+        $this->removePlugin('generic', 'usageStats');
+
+        // Remove views report plugin
+        $this->removePlugin('reports', 'views');
+
+        // It is not needed to remove usageStats plugin scheduled task from the Acron plugin, because
+        // PKPAcronPlugin function _parseCrontab() will be called at the end of update, that
+        // will overwrite the old crontab setting.
+
+        // Remove the old scheduled task from the table scheduled_tasks???
+    }
+
+    /** Remove the plugin from the DB table version and from the file system */
+    protected function removePlugin($category, $productName)
+    {
+        $fileManager = new FileManager();
         $pluginVersionsEntryExists = DB::table('versions')
             ->where('current', 1)
             ->where('product_type', '=', 'plugins.' . $category)
@@ -46,7 +58,7 @@ class I6782_RemovePlugins extends Migration
             $fileManager->rmtree($pluginDest);
             $fileManager->rmtree($pluginLibDest);
             if (is_dir($pluginDest) || is_dir($pluginLibDest)) {
-                $this->_installer->log("Plugin \"plugins.{$category}.{$productName}\" could not be deleted from the file system. This may be a permissions problem. Please make sure that the web server is able to write to the plugins directory (including subdirectories) but don't forget to secure it again later.");
+                $this->_installer->log("Plugin \"plugins.{$category}.{$productName}\" could not be deleted from the file system. This may be a permissions problem. Please make sure that the user running the upgrade script is able to write to the plugins directory (including subdirectories) but don't forget to secure it again later.");
             } else {
                 // Differently to versionDao->disableVersion, we will remove the entry from the table 'versions' and plugin_settings
                 // becase the plugin cannot be used any more
@@ -60,40 +72,6 @@ class I6782_RemovePlugins extends Migration
                 // Do we need to do anything with PluginRegistry?
             }
         }
-
-        // Remove views report plugin
-        $category = 'reports';
-        $productName = 'views';
-        $pluginVersionsEntryExists = DB::table('versions')
-            ->where('product_type', '=', 'plugins.' . $category)
-            ->where('product', '=', $productName)
-            ->exists();
-        if ($pluginVersionsEntryExists) {
-            $pluginDest = Core::getBaseDir() . '/plugins/' . $category . '/' . $productName;
-            $pluginLibDest = Core::getBaseDir() . '/' . PKP_LIB_PATH . '/plugins/' . $category . '/' . $productName;
-            // Delete files
-            // Can we check permissions in the perflight spript?
-            $fileManager->rmtree($pluginDest);
-            $fileManager->rmtree($pluginLibDest);
-            if (is_dir($pluginDest) || is_dir($pluginLibDest)) {
-                $this->_installer->log("Plugin \"plugins.{$category}.{$productName}\" could not be deleted from the file system. This may be a permissions problem. Please make sure that the web server is able to write to the plugins directory (including subdirectories) but don't forget to secure it again later.");
-            } else {
-                // Differently to versionDao->disableVersion, we will remove the entry from the table 'versions'
-                // becase the plugin cannot be used any more.
-                // There were no entries in plugin_settings for the views report plugin.
-                DB::table('versions')
-                    ->where('product_type', '=', 'plugins.' . $category)
-                    ->where('product', '=', $productName)
-                    ->delete();
-                // Do we need to do anything with PluginRegistry?
-            }
-        }
-
-        // It is not needed to remove usageStats plugin scheduled task from the Acron plugin, because
-        // PKPAcronPlugin function _parseCrontab() will be called at the end of update, that
-        // will overwrite the old crontab setting.
-
-        // Remove the old scheduled task from the table scheduled_tasks???
     }
 
     /**
